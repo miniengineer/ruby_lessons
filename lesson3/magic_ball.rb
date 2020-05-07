0e6994313d1032e331b2feee3097a38be97d35d5
miniengineer <samadova.minira@gmail.com>
2020-05-07 12:17:55 +0900

    added comments to magic ball answers

--- lesson3/magic_ball.rb
+++ lesson3/magic_ball.rb
@@ -1,19 +1,26 @@
 asnwers = [
+  # Positive
   "It is certain",
   "It is decidedly so",
   "Without a doubt",
   "Yes — definitely",
   "You may rely on it",
+  
+  # Slightly positive
   "As I see it, yes",
   "Most likely",
   "Outlook good",
   "Signs point to yes",
   "Yes",
+
+  # Neutral
   "Reply hazy, try again",
   "Ask again later",
   "Better not tell you now",
   "Cannot predict now",
   "Concentrate and ask again",
+
+  # Negative
   "Don’t count on it",
   "My reply is no",
   "My sources say no",
