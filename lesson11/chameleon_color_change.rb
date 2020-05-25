b6f0b05b98b0fb504428edacb918e71bc76fa096
miniengineer <samadova.minira@gmail.com>
2020-05-25 14:36:22 +0900

    color changing chameleon class

---
+++ lesson11/chameleon_color_change.rb
@@ -0,0 +1,17 @@
+require_relative "chameleon"
+
+chameleon = Chameleon.new
+
+puts chameleon.change_color("red")
+
+sleep 1
+
+puts chameleon.change_color("green")
+
+sleep 1
+
+puts chameleon.change_color("gray")
+
+sleep 1
+
+puts chameleon.change_color("blue")
