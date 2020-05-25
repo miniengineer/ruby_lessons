815105a0112a0544c36a5f15d665d111197a8001
miniengineer <samadova.minira@gmail.com>
2020-05-25 12:43:01 +0900

    ruby class

---
+++ lesson11/bridge.rb
@@ -0,0 +1,10 @@
+class Bridge
+  def initialize
+    puts "Bridge is build"
+  end
+
+  def open
+    puts "Bridge is open, you can ride"
+  end
+
+end 
---
+++ lesson11/road.rb
@@ -0,0 +1,19 @@
+require_relative "bridge" 
+
+puts "Let's ride!"
+
+sleep 1
+
+puts "Suddenly, there is a river ahead!"
+
+sleep 1
+
+bridge = Bridge.new
+
+sleep 1
+
+bridge.open
+
+sleep 1
+
+puts "Continue our journey!"
