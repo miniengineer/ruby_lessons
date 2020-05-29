0ba108ccb51f5eaa60c6f5ba01d4ca744d231226
miniengineer <samadova.minira@gmail.com>
2020-05-29 18:00:42 +0900

    refactored results_printer to read images from external folder

--- lesson14/result_printer.rb
+++ lesson14/result_printer.rb
@@ -1,5 +1,26 @@
 class ResultPrinter
 
+  def initialize
+    @status_image = []
+
+    current_path = File.dirname(__FILE__)
+    counter = 0
+
+    while counter <= 7 do
+      file_name = current_path + "/viselitsa_data/images/#{counter}.txt"
+      
+      if File.exist?(file_name)
+        f = File.new(file_name)
+        @status_image << f.read
+        f.close
+      else
+        @status_image << "\n[Image not found]\n"
+      end
+
+      counter += 1
+    end
+  end
+
   def print_status(game)
     puts "Word: " + get_word_for_print(game)
 
@@ -31,120 +52,6 @@ class ResultPrinter
   end
 
   def print_viselitsa(errors)
-    case errors
-    when 0
-      puts "
-            _______
-            |/
-            |
-            |
-            |
-            |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-      when 1
-        puts "
-            _______
-            |/
-            |     ( )
-            |
-            |
-            |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-      when 2
-        puts "
-            _______
-            |/
-            |     ( )
-            |      |
-            |
-            |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-      when 3
-        puts "
-            _______
-            |/
-            |     ( )
-            |      |_
-            |        \\
-            |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-      when 4
-        puts "
-            _______
-            |/
-            |     ( )
-            |     _|_
-            |    /   \\
-            |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-      when 5
-        puts "
-            _______
-            |/
-            |     ( )
-            |     _|_
-            |    / | \\
-            |      |
-            |
-            |
-            |
-          __|________
-          |         |
-          "
-  
-      when 6
-        puts "
-            _______
-            |/
-            |     ( )
-            |     _|_
-            |    / | \\
-            |      |
-            |     / \\
-            |    /   \\
-            |
-          __|________
-          |         |
-          "
-      when 7
-        puts "
-            _______
-            |/     |
-            |     (_)
-            |     _|_
-            |    / | \\
-            |      |
-            |     / \\
-            |    /   \\
-            |
-          __|________
-          |         |
-          "
-    end
+    puts @status_image[errors]
   end
 end
