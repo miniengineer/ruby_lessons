90f1bf7979e82037b1ad06e3387005c2f98efd9a
miniengineer <samadova.minira@gmail.com>
2020-09-06 10:42:56 +0900

    refactor from_file method

--- rubytut_2/lesson5/tsutaya_store/movie.rb
+++ rubytut_2/lesson5/tsutaya_store/movie.rb
@@ -16,11 +16,9 @@ class Movie < Item
   end
 
   def self.from_file(file_path)
-    current_path = File.dirname(__FILE__)
-    
-    raise 'Invalid file' unless File.exists? (current_path + file_path)
+    raise 'Invalid file' unless File.exists? file_path
 
-    f = File.new(current_path + file_path)
+    f = File.new(file_path)
     lines = f.readlines.map { |l| l.chomp }
     f.close
 
