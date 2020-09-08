c68bee898d97e5b7428bd5227bca4e98285fc6e2
miniengineer <samadova.minira@gmail.com>
2020-09-08 20:48:07 +0900

    refactor wish list with OOP (class instead of functions)

--- rubytut_2/lesson6/wish_list/wish.rb
+++ rubytut_2/lesson6/wish_list/wish.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Сундук несбывшихся желаний
 # А теперь стремительно напишите для вашего «Сундука желаний» скрипт, который выводит
 
@@ -17,49 +19,22 @@
 # rewrite implementation with OOP
 # make Wish a class
 
-require 'rexml/document'
-require 'date'
-require_relative 'wish'
+class Wish
 
-current_path = File.dirname(__FILE__)
-file_path = current_path + '/wish_list.xml'
+  # receives a node with information about wish
+  # doesn't care about creating a file or where to get the node from
+  def initialize(node)
+    @text = node.text.strip
+    @date = Date.parse(node.attributes['deadline'])
+  end
 
-abort 'File not found' unless File.exists? file_path
+  # check whether task is overdue
+  def overdue
+    @date > Date.today
+  end
 
-f = File.read(file_path, encoding: 'UTF-8')
-
-begin
-  doc = REXML::Document.new(f)
-rescue REXML::ParseException => e
-  puts 'Invalid XML file'
-  abort e.message
-end
-
-current_date = Date.today
-
-# wishes = {
-#   past_wish: [],
-#   future_wish: []
-# }
-
-wish_list = {
-  overdue: [],
-  not_overdue: []
-}
-
-doc.root.elements.each('wish') do |wish|
-  new_wish = Wish.new(wish)
-  wish_list[new_wish.overdue ? :overdue : :not_overdue] << new_wish
-end
-
-puts 'The following wishes must have been fullfilled by today:'
-wish_list[:overdue].each do |wish|
-  puts wish.to_s
-end
-
-puts
-
-puts 'The following wishes are yet to be fulfilled:'
-wish_list[:not_overdue].each do |wish|
-  puts wish.to_s
+  # display task in a deadline: task_title format
+  def to_s
+    "#{@date.to_s}: #{@text}"
+  end
 end
